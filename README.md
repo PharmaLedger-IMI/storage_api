19f0fae9d1650b8fc6f2ebe12107d3e34f5cb56c
Miah Raihan Mahmud Arman <raihan2006i@gmail.com>
2021-04-12 14:32:18 +0100

    Initial APIs setup and configuration

---
+++ README.md
@@ -0,0 +1,5 @@
+# Storage APIs
+This repo will provide following APIs to access or store data (common repository/storage)
+* Object Store APIs
+* File Store APIs
+* Schema Definition APIs
---
+++ backend/index.js
@@ -0,0 +1,36 @@
+let express = require('express')
+    , { default: ParseServer, ParseGraphQLServer, RedisCacheAdapter } = require('parse-server')
+    , redisOptions = {url: process.env.REDIS_URI}
+    , redisCache = new RedisCacheAdapter(redisOptions)
+    , parseServerConfig = {
+      databaseURI: process.env.DATABASE_URI,
+      appId: process.env.APP_ID,
+      restAPIKey: process.env.RESTAPI_KEY,
+      masterKey: process.env.MASTER_KEY,
+      serverURL: process.env.SERVER_URL,
+      publicServerURL: process.env.PUBLIC_SERVER_URL,
+      cloud: "./cloud/main.js",
+      cacheAdapter: redisCache
+    }
+    , parseServer = new ParseServer(parseServerConfig)
+    , app = express();
+
+app.all('/storage/*', function (req, res, next) {
+  if(req.headers['x-storage-application-id']) {
+    req.headers['x-parse-application-id'] = req.headers['x-storage-application-id'];
+  }
+  if(req.headers['x-storage-master-key']) {
+    req.headers['x-parse-master-key'] = req.headers['x-storage-master-key'];
+  }
+  if(req.headers['x-storage-rest-api-key']) {
+    req.headers['x-parse-rest-api-key'] = req.headers['x-storage-rest-api-key'];
+  }
+  next();
+});
+
+app.use('/storage', parseServer.app);
+
+// Start the server
+app.listen(1337, function() {
+  // console.log('Server started!');
+});
